a932b97b0d2566040020c0f1ccd6cb19cd77c53a
fu-qingyun <1366773912@qq.com>
2022-04-14 23:56:03 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+//struct Test
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p;
+////假设p 的值为0x100000。 如下表表达式的值分别为多少？
+////已知，结构体Test类型的变量大小是20个字节
+//int main()
+//{
+//	printf("%p\n", p + 0x1);
+//	printf("%p\n", (unsigned long)p + 0x1);
+//	printf("%p\n", (unsigned int*)p + 0x1);
+//	return 0;
+//}
+
+//int main()
+//{
+//    int a[4] = { 1, 2, 3, 4 };
+//    int* ptr1 = (int*)(&a + 1);
+//    int* ptr2 = (int*)((int)a + 1);
+//    printf("%x,%x", ptr1[-1], *ptr2);
+//    return 0;
+//}
+
